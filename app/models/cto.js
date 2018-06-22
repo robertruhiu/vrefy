acf66b2221fba8cede942493c822508a20c952b4
robertruhiu <robertruhiu@gmail.com>
2018-06-22 16:34:24 +0100

    landing page edits qualified edits

--- app/models/cto.js
+++ app/models/cto.js
@@ -5,10 +5,9 @@ var Schema = mongoose.Schema;
 var ctoSchema = mongoose.Schema({
 
     username: {type: String, required: true},
-    title: {type: String, required: true},
+    company: {type: String, required: true},
     email: {type: String, required: true},
-    phone: {type: String, required: true},
-    country: {type: String, required: true}
+
 
 
 });
--- app/models/devs.js
+++ app/models/devs.js
@@ -6,8 +6,7 @@ var devSchema = mongoose.Schema({
 
     username: {type: String, required: true},
     email: {type: String, required: true},
-    phone: {type: String, required: true},
-    country: {type: String, required: true}
+
 
 
 });
